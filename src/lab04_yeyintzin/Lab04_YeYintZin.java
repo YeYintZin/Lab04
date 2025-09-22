d19517f056965a6d421b510689ecb2beb45839cd
Ye Yint Zin <6289490@edu.vaniercollege.qc.ca>
2025-09-22 09:38:52 -0400

    Update

--- src/lab04_yeyintzin/Lab04_YeYintZin.java
+++ src/lab04_yeyintzin/Lab04_YeYintZin.java
@@ -1,5 +1,10 @@
 package lab04_yeyintzin;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Lab04_YeYintZin extends Application {
@@ -10,7 +15,98 @@ public class Lab04_YeYintZin extends Application {
 
     @Override
     public void start(Stage stage) {
+        stage.setTitle("Business Expenses");
+        GridPane root = new GridPane();
         
+        TextField days = new TextField();
+        Label daysLabel = new Label("Number of days on the trip:");
+        root.add(days, 1, 0);
+        root.add(daysLabel, 0, 0);
+
+        TextField airfare = new TextField();
+        Label airfareLabel = new Label("Amount of airfare:");
+        root.add(airfare, 1, 1);
+        root.add(airfareLabel, 0, 1);
+
+        TextField carRental = new TextField();
+        Label carRentalLabel = new Label("Car Rental fees:");
+        root.add(carRental, 1, 2);
+        root.add(carRentalLabel, 0, 2);
+
+        TextField miles = new TextField();
+        Label milesLabel = new Label("Miles Driven:");
+        root.add(miles, 1, 3);
+        root.add(milesLabel, 0, 3);   
+
+        TextField parking = new TextField();
+        Label parkingLabel = new Label("Parking Fees:");
+        root.add(parking, 1, 4);
+        root.add(parkingLabel, 0, 4);
+
+        TextField taxi = new TextField();
+        Label taxiLabel = new Label("Taxi Fees:");
+        root.add(taxi, 1, 5);
+        root.add(taxiLabel, 0, 5);   
+
+        TextField registration = new TextField();
+        Label registrationLabel = new Label("Conference or Seminar fees:");
+        root.add(registration, 1, 6);
+        root.add(registrationLabel, 0, 6);
+
+        TextField lodging = new TextField();
+        Label lodgingLabel = new Label("Lodging Charges:");
+        root.add(lodging, 1, 7);
+        root.add(lodgingLabel, 0, 7);
+        
+        Label expenses = new Label("");
+        Label allowableExpenses = new Label("");
+        Label excess = new Label("");                   
+        Button button = new Button("Calculate");
+        button.setOnAction(e -> {
+            double totalExpenses = 
+                Integer.parseInt(airfare.getText()) +
+                Integer.parseInt(carRental.getText()) +
+                0.27 *  (Integer.parseInt(miles.getText())) +
+                Integer.parseInt(parking.getText()) +
+                Integer.parseInt(taxi.getText()) +
+                Integer.parseInt(registration.getText()) +
+                Integer.parseInt(lodging.getText());
+            expenses.setText(
+                "The total expense is " +
+                totalExpenses +
+                "$"
+            );
+            
+            double totalAllowed = 162 * Integer.parseInt(days.getText()) + (0.27 * Integer.parseInt(miles.getText()));
+            allowableExpenses.setText(
+                    "The total allowable expense is "
+                    + totalAllowed 
+                    + "$"
+            );
+            
+            if (totalAllowed < totalExpenses) {
+                excess.setText(
+                    "Amount due: " +
+                    (totalExpenses - totalAllowed) +
+                    "$"
+                );
+            } else {
+                excess.setText(
+                    "Amount saved: " +
+                    (totalAllowed - totalExpenses) +
+                    "$"
+                );
+            }
+        });
+        root.add(button, 0, 8);
+        root.add(expenses, 0, 9);
+        root.add(allowableExpenses, 0, 10);
+        root.add(excess, 0, 11);
+        
+        Scene scene = new Scene(root, 900, 600);
+        scene.getStylesheets().add("demo2.css");
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
